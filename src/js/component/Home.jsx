9871d77fe0c35177c699485876077bcdf7f59e0e
MikeGajardo <mikegajardo@gmail.com>
2022-06-21 00:02:44 +0000

    Deploy cards

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 import Navbar from "./Navbar.jsx";
 import Jumbotron from "./Jumbotron.jsx";
-import CardGroup from "./CardGroup.jsx";
+// import CardGroup from "./CardGroup.jsx";
 import Footer from "./Footer.jsx";
+import Cards from "./Cards.jsx";
  
 //create your first component
 const Home = () => {
@@ -13,7 +14,8 @@ const Home = () => {
 		<>
 			<Navbar />
 			<Jumbotron />
-			<CardGroup />
+			{/* <CardGroup /> */}
+			<Cards />
 			<Footer />
 		</>
 	);
